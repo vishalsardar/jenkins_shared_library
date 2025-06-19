607e3d5833dfe26ea9bb7cadf877d2fae46add01
vishalsardar <65338335+vishalsardar@users.noreply.github.com>
2025-06-19 21:32:49 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,16 +1,16 @@
-def call(String imagge, String tag){
+def call(){
   echo "This is Pushing image to DockerHub stage"
   withCredentials([usernamePassword(
       credentialsId: 'dockerHubCred',
       usernameVariable: 'dockerHubUser',
       passwordVariable: 'dockerHubPass'
   )]) {
-      sh 'echo "Logging in to DockerHub"'
-      sh 'docker login -u "$dockerHubUser" -p "$dockerHubPass"'
-      echo "Login successful!!!!"  
-      sh "docker image tag ${image}:${tag} $dockerHubUser/${image}:${tag}"
-      echo "Image tag updated"
-      sh "docker push $dockerHubUser/${image}:${tag}"
-      echo "Image Pushed to DockerHub"
+      sh '''
+          echo "Logging in to DockerHub"
+          docker login -u "$dockerHubUser" -p "$dockerHubPass"
+          docker image tag notes-app:latest "$dockerHubUser/notes-app:latest"
+          docker push "$dockerHubUser/notes-app:latest"
+          echo "Login successfull, Image Pushed to DockerHub"
+      '''
   }
 }
