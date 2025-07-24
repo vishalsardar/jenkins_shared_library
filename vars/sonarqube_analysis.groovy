e0d921ca56c942210ef5f9ca7fb72f7b05506902
vishalsardar <65338335+vishalsardar@users.noreply.github.com>
2025-07-24 18:05:08 +0530

    Create sonarqube_analysis.groovy

---
+++ vars/sonarqube_analysis.groovy
@@ -0,0 +1,5 @@
+call def(String SonarQubeAPI, String ProjectName, String ProjectKey){
+  withSonarQubeEnv("${SonarQubeAPI}"){
+    sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X"
+  }
+}
